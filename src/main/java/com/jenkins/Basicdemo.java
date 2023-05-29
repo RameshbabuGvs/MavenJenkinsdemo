a1f62dfadc65d6084e0b65a6a2355ce8b3758347
Ramesh <rameshbabugvs@gmail.com>
2023-05-29 19:49:33 +0530

    firt commit

---
+++ src/main/java/com/jenkins/Basicdemo.java
@@ -0,0 +1,33 @@
+package com.jenkins;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Basicdemo {
+
+	   @Test
+	   public void testCalculateAppriasal() {
+
+
+	//System.setProperty("webdriver.chrome.driver", "D://Softwares//chromedriver.exe");
+     WebDriverManager.chromedriver().setup();
+	// TODO Auto-generated method stub
+	WebDriver driver = new ChromeDriver();
+	
+	driver.navigate().to("http://registration.ap.gov.in/chatbot-0.0.1-SNAPSHOT/");
+	driver.manage().window().maximize();
+	
+	  driver.findElement(By.xpath("//a[text()='RTI Act']")).click();
+	
+	driver.navigate().back();
+	driver.findElement(By.xpath("(//a[@class='nav-link'])[1]"));
+	//driver.navigate().back();
+	//data added
+	driver.close();
+	}
+	
+}
