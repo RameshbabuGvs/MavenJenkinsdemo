24eeef9479aa59c6010db9e62c8115881a150430
Ramesh <rameshbabugvs@gmail.com>
2023-06-02 16:20:33 +0530

    Code added

--- src/main/java/com/jenkins/Basicdemo.java
+++ src/main/java/com/jenkins/Basicdemo.java
@@ -8,26 +8,17 @@ import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Basicdemo {
+     @Test
+	public void testCalculateAppriasal() {
 
-	   @Test
-	   public void testCalculateAppriasal() {
-
-
-	//System.setProperty("webdriver.chrome.driver", "D://Softwares//chromedriver.exe");
+   //System.setProperty("webdriver.chrome.driver", "D://Softwares//chromedriver.exe");
      WebDriverManager.chromedriver().setup();
 	// TODO Auto-generated method stub
 	WebDriver driver = new ChromeDriver();
-	
 	driver.navigate().to("http://registration.ap.gov.in/chatbot-0.0.1-SNAPSHOT/");
 	driver.manage().window().maximize();
-	
-	  driver.findElement(By.xpath("//a[text()='RTI Act']")).click();
-	
-	driver.navigate().back();
-	driver.findElement(By.xpath("(//a[@class='nav-link'])[1]"));
-	//driver.navigate().back();
-	//data added
+	driver.findElement(By.xpath("//a[text()='RTI Act']")).click();
+
 	driver.close();
 	}
-	
-}
+       }
